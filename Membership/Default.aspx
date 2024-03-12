3e36c850fef92add549107ca6137b95fc527a046
DanHagen <dan.hagen@centralia.edu>
2024-03-12 12:56:12 -0700

    Add project files.

---
+++ Membership/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Membership.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div style="float:right; width:200px; height:70px; border:solid 2px black;padding: 4px 4px 4px 4px;">
+                <asp:TextBox ID="txtUsername" runat= "server" /><br />
+                <asp:TextBox ID="txtPassword" runat= "server" /><br />
+                <asp:Button ID="btnLogin" runat="server" Text="Log In" OnClick="btnLogin_Click" /><asp:Label ID="lblMessage" ForeColor="Red" runat="server" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Membership/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Membership
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string creds = "Server=10.48.38.5; Database=Members;UID=ITUser;PWD=CentraliaIT;";
+            SqlConnection conn = new SqlConnection(creds);
+            SqlCommand cmd = new SqlCommand("LogIn", conn);
+            cmd.CommandType= System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@Password", txtPassword.Text); 
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.HasRows) 
+            { 
+                dr.Read();
+                string Last = dr[0].ToString();
+                string First = dr[1].ToString();
+                Session["Name"] = First + " " + Last;   
+                conn.Close();
+                Response.Redirect("members.aspx");
+            }
+            else
+            {
+                conn.Close();
+                lblMessage.Text = "Hacker!!";
+            }
+          
+           
+
+        }
+    }
+}
